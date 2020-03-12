66133c14e16d5cd90095a87496258614662672ae
Francesco Sisca <franksisca@gmail.com>
2020-03-12 20:45:57 +0100

    inseriti dati per database WORLD remoto

--- connessione-db/src/main/java/it/dstech/connessionedb/ConnessioneDatabase.java
+++ connessione-db/src/main/java/it/dstech/connessionedb/ConnessioneDatabase.java
@@ -7,12 +7,28 @@ import java.sql.SQLException;
 import java.sql.Statement;
 
 public class ConnessioneDatabase {
+	
+	/*
+	 * Username: 2QmHgYocHj
+
+Database name: 2QmHgYocHj
+
+Password: qNujd585zn
+
+Server: remotemysql.com
+
+Port: 3306
+
+These are the username and password to log in to your database and phpMyAdmin
+
+
+	 */
 
 	public static void main(String[] args) throws ClassNotFoundException, SQLException {
 		Class.forName("com.mysql.cj.jdbc.Driver"); // in questo punto carichia nella JVM in esecuzione la nostra libreria 
-		String password ="password"; // la vostra password
-		String username = "root"; // la vostra username
-		String url = "jdbc:mysql://localhost:3306/world?useUnicode=true&useLegacyDatetimeCode=false&serverTimezone=UTC&createDatabaseIfNotExist=true&allowPublicKeyRetrieval=true&useSSL=false";
+		String password ="qNujd585zn"; // la vostra password
+		String username = "2QmHgYocHj"; // la vostra username
+		String url = "jdbc:mysql://remotemysql.com:3306/2QmHgYocHj?useUnicode=true&useLegacyDatetimeCode=false&serverTimezone=UTC&createDatabaseIfNotExist=true&allowPublicKeyRetrieval=true&useSSL=false";
 		Connection connessione = DriverManager.getConnection(url, username, password);
 		Statement statement = connessione.createStatement();
 		ResultSet risultatoQuery = statement.executeQuery("select * from city where CountryCode =  \"ITA\" ;");
